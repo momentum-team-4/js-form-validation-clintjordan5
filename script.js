50c011a246a7ea6fbc730d40e09d695ba4701ab7
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-08-23 18:59:30 -0400

    working on submit button x number of days

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 console.log('Add validation!');
 
+submit-button.addEventListener ("click", function () {
+    submit-button.textContent; "days" * 5 
+    console.log
+})
 
+// attempting to add an event listener so that when clicking submit button, it will multiply the number of days by 5
