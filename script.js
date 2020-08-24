56795e7d43dbf94a049f05cfd7f462b2cfcb271c
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-08-24 16:33:10 -0400

    debugged eventlistener and queryselector with taylor

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 console.log('Add validation!');
 
-submit-button.addEventListener ("click", function () {
-    let days = document.querySelector(days)
-    let cost = (days * 5)
-    submit-button.textContent; (days * 5) ; 
-    console.log
+let totalcost = document.querySelector("#total")
+let submit = document.querySelector("#submit-button")
+
+submit.addEventListener ("click", function () {
+    event.preventDefault()
+    let numberdays = document.querySelector("#days").value
+    let cost = (numberdays * 5)
+    totalcost.textContent= "total parking cost" + " $ " +  cost ; 
 })
 
 // attempting to add an event listener so that when clicking submit button, 
 // it will multiply the number of days by 5
 
-const days = [
-'Sunday',
-'Monday',
-'Tuesday',
-'Wednesday',
-'Thursday',
-'Friday',
-'Saturday'
-]
+// const days = [
+// 'Sunday',
+// 'Monday',
+// 'Tuesday',
+// 'Wednesday',
+// 'Thursday',
+// 'Friday',
+// 'Saturday'
+// ]
 
-const dayIndex = d.getDay()
-const dayName = [dayIndex]
+// const dayIndex = d.getDay()
+// const dayName = [dayIndex]
 
-const formatted = `${dayName}, ${date} ${monthName} ${year}`
-console.log(formatted)
+// const formatted = `${dayName}, ${date} ${monthName} ${year}`
+// console.log(formatted)
 
 // I am thinking - make an array with date names. 
 // admittedly not sure where to go with lines 21-25 in js, testing the code from Date Object Article in CSS Tricks
