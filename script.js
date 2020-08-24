88086a63d75816a65173eac48e90e31e5a8dd4a0
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-08-24 13:51:43 -0400

    debugged CVV field, fixing JS event listener

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 console.log('Add validation!');
 
 submit-button.addEventListener ("click", function () {
-    submit-button.textContent; "days" * 5 
+    let days = document.querySelector(days)
+    let cost = (days * 5)
+    submit-button.textContent; (days * 5) ; 
     console.log
 })
 
@@ -28,3 +30,4 @@ console.log(formatted)
 // admittedly not sure where to go with lines 21-25 in js, testing the code from Date Object Article in CSS Tricks
 // Ideally, write code so that "If date name is Saturday or Sunday, price is $7 rather than $5"
 // need to find way to apply this array to the days field (line 30 html) and submit button (line 55 html)
+
